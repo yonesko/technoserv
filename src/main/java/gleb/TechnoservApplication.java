35bb0d22085c51944a4f44b4aa577e59cdfedc2b
User <kuk123>
2017-08-11 22:17:45 +0300

    RSS integration

--- src/main/java/gleb/TechnoservApplication.java
+++ src/main/java/gleb/TechnoservApplication.java
@@ -2,11 +2,14 @@ package gleb;
 
 import gleb.data.WordsRepo;
 import gleb.data.WordsRepoTest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("integration.xml")
 public class TechnoservApplication {
     public static void main(String[] args) {
         SpringApplication.run(TechnoservApplication.class, args);
@@ -15,5 +18,12 @@ public class TechnoservApplication {
     @Bean
     WordsRepo wordsRepoTest() {
         return new WordsRepoTest();
+    }
+
+    @Bean
+    CommandLineRunner initSolr() {
+        return (args) -> {
+
+        };
     }
 }
