228435554eedd1d760ba923b1b2b400dff82d66c
User <kuk123>
2017-08-13 11:26:06 +0300

    front end

--- src/main/resources/static/app.config.js
+++ src/main/resources/static/app.config.js
@@ -8,7 +8,8 @@ angular.module('phonecatApp').config(['$locationProvider', '$routeProvider',
             templateUrl: 'wordsStat.html',
             controller: 'WordsStatController'
         }).when('/wordDetail/:phoneId', {
-            templateUrl: 'wordDetail.html'
+            templateUrl: 'wordDetail.html',
+            controller: 'WordDetailController'
         }).otherwise('/wordsStat');
     }
 ]).controller('WordsStatController', function ($scope) {
@@ -16,4 +17,6 @@ angular.module('phonecatApp').config(['$locationProvider', '$routeProvider',
         'adam': 1,
         "apple": 34
     }
+}).controller('WordDetailController', function ($scope, $routeParams) {
+    $scope.params = $routeParams
 });
