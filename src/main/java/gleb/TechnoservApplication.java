b6f108c59aede1e881063ba83645e969b71b8143
User <kuk123>
2017-08-13 18:54:01 +0300

    front end; fix

--- src/main/java/gleb/TechnoservApplication.java
+++ src/main/java/gleb/TechnoservApplication.java
@@ -1,21 +1,11 @@
 package gleb;
 
-import gleb.data.NewsItem;
 import gleb.data.WordsRepo;
 import gleb.data.WordsRepoSolr;
-import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.apache.solr.common.SolrInputDocument;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
-
-import java.time.Duration;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @ImportResource("integration.xml")
@@ -27,30 +17,5 @@ public class TechnoservApplication {
     @Bean
     WordsRepo wordsRepoTest() {
         return new WordsRepoSolr();
-    }
-
-//    @Bean
-    CommandLineRunner initSolr() {
-        return (args) -> {
-            SolrClient solr = new HttpSolrClient.Builder("http://localhost:8983/solr/newsitem").build();
-
-            solr.deleteByQuery("*:*");
-
-//            List<NewsItem> newsItemList = new ArrayList<>();
-//
-//            newsItemList.add(new NewsItem("1", "Прохоров продал семь процентов «Русала» по цене ниже рыночной", ZonedDateTime.now().minus(Duration.ofDays(1))));
-//            newsItemList.add(new NewsItem("2", "Прохоров продал семь процентов «Русала» по цене ниже рыночной", ZonedDateTime.now().minus(Duration.ofDays(2))));
-//            newsItemList.add(new NewsItem("4", "Прохоров продал семь процентов «Русала» по цене ниже рыночной", ZonedDateTime.now().minus(Duration.ofDays(2))));
-//            newsItemList.add(new NewsItem("5", "В туриндустрии отреагировали на решение России об опасности отдыха в Турции", ZonedDateTime.now().minus(Duration.ofDays(3))));
-//
-//            for (NewsItem newsItem : newsItemList) {
-//                SolrInputDocument doc = new SolrInputDocument();
-//                doc.addField("content_txt_en", newsItem.getContent());
-//                doc.addField("publication_dt", newsItem.getPublicationDateTime().toInstant().toString());
-//                doc.addField("id", newsItem.getId());
-//                solr.add(doc);
-//            }
-//            solr.commit();
-        };
     }
 }
