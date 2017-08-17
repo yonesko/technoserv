9fecd1abf32ee8cc63de95ccc456ecf8c90a3b54
gdanichev <gitgle66>
2017-08-17 13:31:16 +0300

    refach

--- src/main/java/gleb/TechnoservApplication.java
+++ src/main/java/gleb/TechnoservApplication.java
@@ -4,38 +4,34 @@ import gleb.config.SolrIndexingServiceActivator;
 import gleb.data.WordsRepo;
 import gleb.data.WordsRepoSolr;
 import org.apache.log4j.Logger;
+import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class TechnoservApplication {
     private static final Logger LOG = Logger.getLogger(TechnoservApplication.class);
-    @Autowired
-    private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(TechnoservApplication.class, args);
     }
 
     @Bean
-    public WordsRepo wordsRepo() {
-        return new WordsRepoSolr(solrClient());
+    public WordsRepo wordsRepo(SolrClient solrClient) {
+        return new WordsRepoSolr(solrClient);
     }
 
     @Bean
-    public HttpSolrClient solrClient() {
-        String url = env.getProperty("solr.url");
+    public HttpSolrClient solrClient(@Value("${solr.url}") String url) {
         LOG.info(String.format("Solr url=%s", url));
         return new HttpSolrClient.Builder(url).build();
     }
 
     @Bean
-    public SolrIndexingServiceActivator solrIndexingServiceActivator() {
-        return new SolrIndexingServiceActivator(solrClient());
+    public SolrIndexingServiceActivator solrIndexingServiceActivator(SolrClient solrClient) {
+        return new SolrIndexingServiceActivator(solrClient);
     }
 }
--- src/main/resources/static/app.module.js
+++ src/main/resources/static/app.module.js
@@ -3,6 +3,4 @@
 angular.module('words-popularity-monitor', [
     'ngRoute',
     'chart.js'
-]).factory('WordsStatResource', ['$resource', function ($resource) {
-    return $resource('/wordStat', {}, {})
-}]);
+]);
